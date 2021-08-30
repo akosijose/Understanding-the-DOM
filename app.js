eb453921f449abc554e211c1ab522e3015c2e7e5
akosijose <josegeraldlumbao0307@gmail.com>
2021-08-30 23:25:40 +0800

    lesson 5 code

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let books = document.querySelectorAll("#book-list li .name");
 
 Array.from(books).forEach((book) => {
-  console.log(book);
+  // should return text content in the console
+  //console.log(book.textContent); // grab the text within this HTML element
+  //book.textContent = "hello"; // change the text content inside the html element
+  //book.textContent += " (book title)"; // append another text
 });
-// textContent
+
+// innerHTML
+const bookList = document.querySelector("#book-list");
+// bookList.innerHTML = "<h2>Hello innerHTML</h2>";
+bookList.innerHTML += "<p>Hello innerHTML</p>";
