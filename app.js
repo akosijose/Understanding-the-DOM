1bde77024cc29700a25cf00015b051fb0d3cebc4
akosijose <josegeraldlumbao0307@gmail.com>
2021-08-30 23:06:17 +0800

    lesson 4 code

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const wrap = document.querySelector("#book-list li:nth-child(2) .name");
 let book = document.querySelector("#book-list li .name"); // only return one element
 // console.log(books);
 
-let books = document.querySelectorAll("#book-list li .name"); // return collection of elements
+let books = document.querySelectorAll("#book-list li .name"); // return node list
 // console.log(books);
 
 // array method using forEach
 Array.from(books).forEach((book) => {
+  // return collection of elements
   console.log(book);
 });
