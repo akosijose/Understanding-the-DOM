8aac1e1189d5c6fc21424238477e9f7d4396fa4f
akosijose <josegeraldlumbao0307@gmail.com>
2021-08-30 23:04:03 +0800

    lesson 5 code

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-// $('#wrapper') // in jQuery
-const wrap = document.querySelector("#book-list li:nth-child(2) .name");
-// console.log(wrap);
+let books = document.querySelectorAll("#book-list li .name");
 
-let book = document.querySelector("#book-list li .name"); // only return one element
-// console.log(books);
-
-let books = document.querySelectorAll("#book-list li .name"); // return collection of elements
-// console.log(books);
-
-// array method using forEach
 Array.from(books).forEach((book) => {
   console.log(book);
 });
+// textContent
