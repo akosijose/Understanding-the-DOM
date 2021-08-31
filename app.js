4902253f9f8619520c1d5ab62480d2e106fb69fe
akosijose <josegeraldlumbao0307@gmail.com>
2021-08-31 11:45:33 +0800

    lesson 8 code

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const bookList = document.querySelector("#book-list");
 
-console.log("the parent node is:", bookList.parentNode); // to show the parent node is
-console.log("the parent element is:", bookList.parentElement); // to show the parent elament is
-console.log("the parent element is:", bookList.parentElement.parentElement); // parent element and the parent element of this element
+// log the next siblings
+console.log("book-list next sibling is:", bookList.nextSibling);
+console.log("book-list next element sibling is:", bookList.nextElementSibling);
+// log the previous siblings
+console.log("book-list previous sibling is:", bookList.previousSibling);
+console.log(
+  "book-list previous element sibling is:",
+  bookList.previousElementSibling
+);
 
-// console.log(bookList.childNodes); // show the children element and line break
-console.log(bookList.children); // recommended
+// example of complex how to navigate previous element and access it
+bookList.previousElementSibling.querySelector("p").innerHTML +=
+  "<br /> Hello previous sibling";
