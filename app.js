09246ed68652d34826c7f03c64e67ee4327b5569
akosijose <josegeraldlumbao0307@gmail.com>
2021-09-02 23:33:03 +0800

    lesson 11 code

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-// var btns = document.querySelectorAll("#book-list .delete");
-
-// // btns.addEventListener // we can't do that - we need to loop that element
-// // transfer this element to an Array.from
-
-// Array.from(btns).forEach((btn) => {
-//   btn.addEventListener("click", (e) => {
-//     // navigate outward to the parent element to grab that LI
-//     // console.log(e.target.parentElement);
-//     const li = e.target.parentElement;
-//     li.parentElement.removeChild(li);
-//   });
-// });
-
 const list = document.querySelector("#book-list ul");
 
+// delete books
 list.addEventListener("click", (e) => {
   if (e.target.className === "delete") {
     const li = e.target.parentElement;
     list.removeChild(li);
   }
+});
+
+// add book list
+const addForm = document.forms["add-book"];
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
 });
