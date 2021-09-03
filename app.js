243da040b5dd640040a9f83a3c524144ac0aad8b
akosijose <josegeraldlumbao0307@gmail.com>
2021-09-03 10:37:33 +0800

    lesson 14 code

--- app.js
+++ app.js
@@ -1,37 +1,18 @@
-const list = document.querySelector("#book-list ul");
+// changing attributes
+var book = document.querySelector("li:first-child .name");
+console.log(book);
 
-// delete books
-list.addEventListener("click", (e) => {
-  if (e.target.className === "delete") {
-    const li = e.target.parentElement;
-    list.removeChild(li);
-  }
-});
+// check attributes
+book.hasAttribute("class"); //shoud return TRUE
 
-// add book list
-const addForm = document.forms["add-book"];
+// get attributes
+book.getAttribute("class"); // should return name
+book.getAttribute("id"); // should return null
 
-addForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const value = addForm.querySelector('input[type="text"]').value;
+// set attributes
+book.setAttribute("id", "name"); // should return id attribute and set to 'name'
+book.setAttribute("class", "name"); // should return class attribute and set to 'name'
 
-  // create elements
-  const li = document.createElement("li");
-  const bookName = document.createElement("span");
-  const deleteBtn = document.createElement("span");
-
-  // add content
-  deleteBtn.textContent = "delete";
-  bookName.textContent = value;
-
-  // add classes
-  bookName.classList.add("name");
-  deleteBtn.classList.add("delete");
-
-  // appeend to document inside the LI element
-  li.appendChild(bookName);
-  li.appendChild(deleteBtn);
-
-  // append the LI to the DOM
-  list.appendChild(li);
-});
+// remove attributes
+book.removeAttribute("class"); // remove class attribute
+book.removeAttribute("id"); // remove id attribute
